e2c1cdcab16728d71343d9e78fe8d9726a3c2378
mahdi_nejadsahebi <m.nejadsahebi@live.co.uk>
2018-10-24 09:56:46 -0800

    Version 1.2.0 : The colliders of the keys have been improved. The material of the black kies is also changed. The size of piano is fit now.

--- Assets/Plugins/SystemVersion.cs
+++ Assets/Plugins/SystemVersion.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//Stable
+//97-08-02	1.2.0		The colliders of the keys have been improved. The material of the black kies is also changed. The size of piano is fit now. 
 
 public class SystemVersion : MonoBehaviour {
     public static int MAJOR_VERSION = 1;
-    public static int MINOR_VERSION = 1;
-    public static int BUILD_VERSION = 3;
+    public static int MINOR_VERSION = 2;
+    public static int BUILD_VERSION = 0;
 }
