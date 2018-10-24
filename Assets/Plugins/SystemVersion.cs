f2e8715a8a4fbb90f94c9b33766e9ea34f2ea1ad
mahdi_nejadsahebi <m.nejadsahebi@live.co.uk>
2018-10-23 16:32:58 -0800

    Version1.1.3 : Stable

--- Assets/Plugins/SystemVersion.cs
+++ Assets/Plugins/SystemVersion.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Stable
+
 public class SystemVersion : MonoBehaviour {
     public static int MAJOR_VERSION = 1;
-    public static int MINOR_VERSION = 0;
-    public static int BUILD_VERSION = 0;
+    public static int MINOR_VERSION = 1;
+    public static int BUILD_VERSION = 3;
 }
