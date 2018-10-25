d6f713a52409ea5e1fde18e2cc9a28c8391264e2
mahdi_nejadsahebi <m.nejadsahebi@live.co.uk>
2018-10-24 16:34:56 -0800

    All notes of piano have been implemented.

--- Assets/Plugins/SystemVersion.cs
+++ Assets/Plugins/SystemVersion.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//97-08-02	1.2.2		Sounds will not play again untill the animation of releasing is not over. 
+//97-08-02	1.2.4		All notes of piano have been implemented.
 
 public class SystemVersion : MonoBehaviour {
     public static int MAJOR_VERSION = 1;
