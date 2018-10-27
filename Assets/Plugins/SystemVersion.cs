bf4eca8e935fd2b23979c867edf434f58ad21c85
mahdi_nejadsahebi <m.nejadsahebi@live.co.uk>
2018-10-27 14:44:43 -0800

    Version 1.4.1 : Global game controller and animation have been set for whole of the kies.

--- Assets/Plugins/SystemVersion.cs
+++ Assets/Plugins/SystemVersion.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//97-08-05	1.4.0		Global game controller and animation have been set for white kies.
+//97-08-05	1.4.1		Global game controller and animation have been set for whole of the kies.
 
 public class SystemVersion : MonoBehaviour {
     public static int MAJOR_VERSION = 1;
     public static int MINOR_VERSION = 4;
-    public static int BUILD_VERSION = 0;
+    public static int BUILD_VERSION = 1;
 }
