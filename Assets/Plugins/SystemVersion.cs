fe500b01d39dd2745ce369b2cf48067b0651b8c1
mahdi_nejadsahebi <m.nejadsahebi@live.co.uk>
2018-10-27 09:59:34 -0800

    Version 1.3.0 : The material fading on black keys has been done.

--- Assets/Plugins/SystemVersion.cs
+++ Assets/Plugins/SystemVersion.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//97-08-02	1.2.4		All notes of piano have been implemented.
+//97-08-02	1.3.0		The material fading on black keys has been done.
 
 public class SystemVersion : MonoBehaviour {
     public static int MAJOR_VERSION = 1;
-    public static int MINOR_VERSION = 2;
-    public static int BUILD_VERSION = 2;
+    public static int MINOR_VERSION = 3;
+    public static int BUILD_VERSION = 0;
 }
