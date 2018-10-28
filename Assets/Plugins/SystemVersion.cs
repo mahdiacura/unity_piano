e4e4564d7e0d64c6d091608c2e93ae05f8e58161
mahdi_nejadsahebi <m.nejadsahebi@live.co.uk>
2018-10-28 10:22:12 -0800

    Version 1.5.1 : Three musical notes will generate. The material animation of the white kies have been removed.

--- Assets/Plugins/SystemVersion.cs
+++ Assets/Plugins/SystemVersion.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//97-08-06	1.5.0		Musical note is synchronised with the kies. 
+//97-08-06	1.5.1		Three musical notes will generate. The material animation of the white kies have been removed. 
 
 public class SystemVersion : MonoBehaviour {
     public static int MAJOR_VERSION = 1;
     public static int MINOR_VERSION = 5;
-    public static int BUILD_VERSION = 0;
+    public static int BUILD_VERSION = 1;
 }
