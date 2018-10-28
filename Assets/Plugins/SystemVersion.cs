4b287100f299dc428ce383890b78e8948dda45e1
mahdi_nejadsahebi <m.nejadsahebi@live.co.uk>
2018-10-28 15:34:02 -0800

    Version 1.5.3 : Play button is in progress.

--- Assets/Plugins/SystemVersion.cs
+++ Assets/Plugins/SystemVersion.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//97-08-06	1.5.2		When the scene is crowed, the sounds will play with a little delay.
+//97-08-06	1.5.3		Play button is in progress.
 
 public class SystemVersion : MonoBehaviour {
     public static int MAJOR_VERSION = 1;
     public static int MINOR_VERSION = 5;
-    public static int BUILD_VERSION = 2;
+    public static int BUILD_VERSION = 3;
 }
